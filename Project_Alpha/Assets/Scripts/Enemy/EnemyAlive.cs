402acc05b82a793a38cb69c5055959bb0b2b82a0
ABucciarelli-CR <alessiobuccia@gmail.com>
2017-12-01 17:04:21 +0100

    _ il nemico con scudo attacca... a 360 gradi_

--- Project_Alpha/Assets/Scripts/Enemy/EnemyAlive.cs
+++ Project_Alpha/Assets/Scripts/Enemy/EnemyAlive.cs
@@ -6,7 +6,7 @@ public class EnemyAlive : MonoBehaviour
 {
 
 	public float speed = .01f;
-    public float life = -1;
+    public float life = 0;
 
     public GameObject deadBody;
 
---
+++ Project_Alpha/Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Enemy
+{
+    public class EnemyStateMachine : MonoBehaviour
+    {
+
+        [HideInInspector] public EnemyState enemyState;
+
+        
+        public float speed = .01f;
+        public int damage;
+        public float attackDelay;
+        public ContactFilter2D contactFilter;
+        [HideInInspector] public Vector2 movement;
+        [HideInInspector] public float direction = 1;
+        [HideInInspector] public float timeToChangeDirection = 5f;
+        [HideInInspector] public float ttcd;
+        /*[HideInInspector]*/ public Collider2D[] hitColliders;
+        [HideInInspector] public int maxArray = 100;
+
+
+        public enum EnemyState
+        {
+            idle,
+            attack,
+            searchPlayer
+        }
+
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+            enemyState = EnemyState.idle;
+        }
+
+        void Update()
+        {
+
+            switch (enemyState)
+            {
+                case EnemyState.idle:
+                    Idle();
+                    break;
+
+                case EnemyState.attack:
+                    Attack();
+                    break;
+
+                case EnemyState.searchPlayer:
+                    SearchPlayer();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        public virtual void Idle()
+        { }
+
+        public virtual void Attack()
+        { }
+
+        public virtual void SearchPlayer()
+        { }
+
+    }
+}
