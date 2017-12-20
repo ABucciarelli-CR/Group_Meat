d8ab21da1371f17b03e7ef56093a59c974ee28b9
Bucciarelli Alessio <alessiobuccia@gmail.com>
2017-12-20 12:24:13 +0100

    _cambiata sprite del player e aggiunto bottone per il restart del livello (P)_

---
+++ Project_Alpha/Assets/Scripts/Global/ExtraButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameManagerAction))]
+public class ExtraButton : MonoBehaviour
+{
+    private bool restartLevel = false;
+    private GameManagerAction gameManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameManager = GetComponent<GameManagerAction>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        restartLevel = Input.GetButtonDown("Restart");
+        gameManager.RestartTheLevel(restartLevel);
+        
+    }
+}
