6e51e6e1a61f2eed5bf7d6411f3bb90ac90cd4d7
Bucciarelli Alessio <alessiobuccia@gmail.com>
2018-01-20 07:18:56 +0100

    _fatta la sporta_

---
+++ Project_Alpha/Assets/Scripts/ScenarioObject/NormalDoor/OnlyCloseDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyCloseDoor : MonoBehaviour
+{
+    public GameObject door;
+    public LayerMask playerLayerMask;
+    public bool thisDoorClosed = false;
+    
+    private RaycastHit2D hitRight;
+    private float maxDistance = 5;
+
+    void Start()
+    {
+        hitRight = new RaycastHit2D();
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        door.SetActive(false);
+        playerLayerMask = (1 << LayerMask.NameToLayer("player")) | (1 << LayerMask.NameToLayer("midGhost"));
+    }
+
+    private void Update()
+    {
+        //Debug.Log(playerLayerMask.value);
+        hitRight = Physics2D.Raycast(transform.position, -Vector2.left, maxDistance, playerLayerMask);
+
+        if (hitRight.collider != null)
+        {
+            //Debug.Log("Here!1");
+            if (hitRight.collider.CompareTag("Player"))
+            {
+                door.SetActive(true);
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            door.SetActive(true);
+        }
+    }
+}
