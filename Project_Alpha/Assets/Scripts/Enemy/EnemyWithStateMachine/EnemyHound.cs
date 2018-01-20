e5c8cf56d5cfd052f423f3125ddd40a9578217e4
ABucciarelli-CR <alessiobuccia@gmail.com>
2018-01-20 13:12:01 +0100

    _hound in working_

---
+++ Project_Alpha/Assets/Scripts/Enemy/EnemyWithStateMachine/EnemyHound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyStateMachine))]
+[RequireComponent(typeof(EnemyHealth))]
+public class EnemyHound : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
