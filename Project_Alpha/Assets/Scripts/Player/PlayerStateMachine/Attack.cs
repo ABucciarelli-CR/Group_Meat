0cca89ad654c7354cdc1ae41be6d93cdba2d294e
Bucciarelli Alessio <alessiobuccia@gmail.com>
2018-02-17 23:35:37 +0100

    _aggiunta la possibilità di dare un delay per l'attivazione della frenzy se questa ha un audio, piccole modifiche alla camera in modo che segua il player in modo un pelo fluido_

--- Project_Alpha/Assets/Scripts/Player/PlayerStateMachine/Attack.cs
+++ Project_Alpha/Assets/Scripts/Player/PlayerStateMachine/Attack.cs
@@ -21,7 +21,7 @@ public class Attack : MonoBehaviour
     public bool frenzyActive = false;
     [ReadOnly]
     public bool abilityEnded = false;
-
+    [ReadOnly]
     public AudioSource audioSource;
 
     [Title("Modifiche abilitate.")]
@@ -30,11 +30,17 @@ public class Attack : MonoBehaviour
     public int healForDamage = 2;
 
     [Title("Valori della frenzy.")]
-    [InfoBox("Consumo vita al secondo")]
+    [InfoBox("Consumo vita al secondo(valore massimo 10)")]
+    [MaxValue(10)]
     public float maxHealthConsume = 5f;
     [InfoBox("Durata(sec)")]
     [MinValue(0)]
     public float maxFrenzyTime = 5f;
+    [InfoBox("Attivazione Audio durante la frenzy")]
+    public bool frenzyAudioOn = true;
+    [EnableIf("frenzyAudioOn")]
+    [InfoBox("Se attivo audio, il tempo da impostare prima che parta la frenzy")]
+    public float waitTimeToStartFrenzy = 6.7f;
 
     [Title("Modificatori frenzy.")]
     [InfoBox("valore minimo 1, il moltiplicatore funziona secondo la seguente formula: attacco * moltiplicatore")]
@@ -47,8 +53,8 @@ public class Attack : MonoBehaviour
     private DamageAreaCollider damageAreaCollider;
     private GameObject frenziPlaceholder;
     private float damageDeltaTime = 0;
-    private bool memeMusicOn = true;
-    private bool endWaitMemeMusic = false;
+    
+    private bool endWaitMusic = false;
     private GlobalVariables globalVariables;
     //private Animator anim;
 
@@ -77,9 +83,9 @@ public class Attack : MonoBehaviour
     {
         if (frenzyActive)
         {
-            if (memeMusicOn)
+            if (frenzyAudioOn)
             {
-                if (endWaitMemeMusic)
+                if (endWaitMusic)
                 {
                     damageDeltaTime += (maxHealthConsume * Time.fixedDeltaTime);
                     if (damageDeltaTime >= 1)
@@ -181,9 +187,9 @@ public class Attack : MonoBehaviour
             //startAndEnd[0] = 4;
             //startAndEnd[0] = 10;
 
-            if(memeMusicOn)
+            if(frenzyAudioOn)
             {
-                StartCoroutine(StartingMusic(6.7f));
+                StartCoroutine(StartingMusic(waitTimeToStartFrenzy));
             }
             else
             {
@@ -204,10 +210,10 @@ public class Attack : MonoBehaviour
 
     IEnumerator StartingMusic(float sec)
     {
-        endWaitMemeMusic = false;
+        endWaitMusic = false;
         audioSource.Play();
         yield return new WaitForSeconds(sec);
-        endWaitMemeMusic = true;
+        endWaitMusic = true;
         CreateAndActivateFrenzyPlaeholder();
         frenziPlaceholder.SendMessage("Loop"/*, startAndEnd*/);
         StartCoroutine(EndAbility(maxFrenzyTime));
