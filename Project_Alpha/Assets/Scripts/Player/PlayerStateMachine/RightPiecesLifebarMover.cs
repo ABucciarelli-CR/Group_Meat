6d2c9b8b8b19ece27808a4fcd5cbfa3c9aafc5c4
Bucciarelli Alessio <alessiobuccia@gmail.com>
2018-04-21 03:35:44 +0200

    _healthbar non conclusa_

---
+++ Project_Alpha/Assets/Scripts/Player/PlayerStateMachine/RightPiecesLifebarMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
+
+public class RightPiecesLifebarMover : MonoBehaviour
+{
+    public Slider realHealth;
+    public GameObject startLifebarPoint;
+    private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startingPosition = gameObject.transform.position;
+    }
+    // * realHealth.value
+    // Update is called once per frame
+    void Update ()
+    {
+        float toUnit = ((1 / realHealth.maxValue) * realHealth.value);
+        Debug.Log((startLifebarPoint.transform.position.x - startingPosition.x) * toUnit);
+        gameObject.transform.position = new Vector2((startLifebarPoint.transform.position.x - startingPosition.x) * toUnit, gameObject.transform.position.y);
+	}
+}
